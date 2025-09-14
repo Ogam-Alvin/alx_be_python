e0201e36c02bb9a732495148e654616981502e54
Alvin Ogam <alvinogam@gmail.com>
2025-09-14 19:02:23 +0300

    Second task complete

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+#Define the variables to be used
+principal = 1000 #represents $1000
+rate = 0.05 #represents 5%
+time = 3 #represents 3 years
+
+#Calculate the simple interest
+interest = principal*rate*time 
+
+#Display the results of the Calculations
+print(f'The simple interest is {interest}')
