b3905f28dc44ce0e7c8027e3dcb425c598854202
Alvin Ogam <alvinogam@gmail.com>
2025-09-14 19:28:03 +0300

    Completed task 5

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,8 @@
+#Prompt the user to enter their age
+age = int(input('How old are you '))
+
+#Calculate the future age
+future_age = (2050 - 2023) + age
+
+#Display the future age
+print(f'In 2050, you will be {future_age} years old')
