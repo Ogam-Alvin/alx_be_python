5daa974078c63c13d2a146196de4e929c6b4a626
Alvin Ogam <alvinogam@gmail.com>
2025-09-14 19:44:03 +0300

    Modified task 4

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -4,8 +4,8 @@ minutes_in_an_hour = 60 #How many minutes that make a hour
 seconds_in_a_minute = 60 # Number of seconds in a minute
 
 #Perform the conversion of hours to minutes
-conversion1 = hours * minutes_in_an_hour #This will convert the 2 hours into minutes
-seconds = conversion1 * seconds_in_a_minute #This is the end product of the conversion from hours to seconds
+minutes_to_seconds = minutes_in_an_hour * seconds_in_a_minute #This is the conversion of minutes to seconds
+seconds = hours * minutes_to_seconds #This is the conversion of hours to seconds
 
 #Display the results
 print(f'{hours} hour(s) is {seconds} seconds')
