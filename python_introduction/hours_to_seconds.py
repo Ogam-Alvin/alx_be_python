a6b2df4ec12bc49379f0aabd81aaa52924a57536
Alvin Elias Ogam Asingo <alvinogam@gmail.com>
2025-12-22 14:59:56 +0300

    cHANGES

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,11 +1,12 @@
 #Define the variables
 hours = 2
-minutes_in_an_hour = 60 #How many minutes that make a hour
-seconds_in_a_minute = 60 # Number of seconds in a minute
+# minutes_in_an_hour = 60 #How many minutes that make a hour
+# seconds_in_a_minute = 60 # Number of seconds in a minute
 
 #Perform the conversion of hours to minutes
-minutes_to_seconds = minutes_in_an_hour * seconds_in_a_minute #This is the conversion of minutes to seconds
-seconds = hours * minutes_to_seconds #This is the conversion of hours to seconds
+# minutes_to_seconds = minutes_in_an_hour * seconds_in_a_minute #This is the conversion of minutes to seconds
+# seconds = hours * minutes_to_seconds #This is the conversion of hours to seconds
+seconds = hours * 3600
 
 #Display the results
 print(f'{hours} hour(s) is {seconds} seconds')
